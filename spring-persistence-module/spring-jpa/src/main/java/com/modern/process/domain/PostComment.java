b6690e6c6fd1acf092fd48d877c32443f02e1ca9
sharifi <h_sharifi@modernisc.com>
2022-02-13 17:53:16 +0330

    set generation value for id as SEQUENCE

--- spring-persistence-module/spring-jpa/src/main/java/com/modern/process/domain/PostComment.java
+++ spring-persistence-module/spring-jpa/src/main/java/com/modern/process/domain/PostComment.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class PostComment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(name = "post_review", length = 50, nullable = false)
