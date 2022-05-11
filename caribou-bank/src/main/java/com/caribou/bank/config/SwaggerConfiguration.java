a593811915afe5223cb4526090b5ff01748f1cb1
sharifi <h_sharifi@modernisc.com>
2022-05-11 09:27:44 +0430

    rename class name

--- caribou-bank/src/main/java/com/caribou/bank/config/SwaggerConfiguration.java
+++ caribou-bank/src/main/java/com/caribou/bank/config/SwaggerConfiguration.java
@@ -8,7 +8,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfiguration {
 
     @Bean
     public Docket api(){
