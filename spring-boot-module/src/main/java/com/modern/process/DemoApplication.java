e857740112695a9f2cece805c24255cbcce1bdb1
sharifi <h_sharifi@modernisc.com>
2022-05-24 18:15:13 +0430

    AOP

--- spring-boot-module/src/main/java/com/modern/process/DemoApplication.java
+++ spring-boot-module/src/main/java/com/modern/process/DemoApplication.java
@@ -1,13 +1,26 @@
 package com.modern.process;
 
+import com.modern.process.config.SampleAdder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-public class DemoApplication {
+@ImportResource("classpath:springAop-applicationContext.xml")
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	SampleAdder sampleAdder;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(sampleAdder.add(2,3));
+	}
 }
---
+++ spring-boot-module/src/main/java/com/modern/process/config/SampleAdder.java
@@ -0,0 +1,12 @@
+package com.modern.process.config;
+
+public class SampleAdder {
+
+    public int add(int a, int b) {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Make sure all the arguments are greater than zero.");
+        }
+        return a + b;
+    }
+
+}
